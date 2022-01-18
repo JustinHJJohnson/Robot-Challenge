276879e4f69d57d28c29635670ff14dd08edfa4c
JustinJohnson <justin.h.j.johnson@gmail.com>
2022-01-19 00:12:45 +1100

    Finished up code, added ReadMe and gitignore files

--- Main.py
+++ Main.py
@@ -3,43 +3,43 @@ import Robot
 
 def main():
     
-    robots = []
-    activeRobot = ()
-    numRobots = 0
+    robots = []         # A list of all the active robots
+    activeRobot = ()    # The index of the current active robot
     
+    # Open and read in the input file
     input = open('input.txt', 'r')
     lines = input.readlines()
 
     # Loop over every instruction in the input file
     for line in lines:
-        # Check if the place command has been run yet, if not check if the current command is place
+        # Check if the place command has been run yet, if not check if
+        # the current command is place
         if len(robots) == 0 and not "PLACE" in line:
             continue
         
         # Parse the current instruction
         if "PLACE" in line:
-            print("Ran place")
+            #print(f"Ran place with {int(line[6])} {int(line[8])} {line[10:].strip()}")
             temp = Robot.Robot()
-            temp.place(int(line[6]), int(line[8]), line[10])
+            temp.place(int(line[6]), int(line[8]), line[10:].strip())
             robots.append(temp)
-            numRobots += 1
-            activeRobot = numRobots
-        if "MOVE" in line:
-            print("Ran move")
-            robots[activeRobot - 1].move()
-        if "LEFT" in line:
-            print("Ran left")
-            robots[activeRobot - 1].rotate('L')
-        if "RIGHT" in line:
-            print("Ran right")
-            robots[activeRobot - 1].rotate('R')
-        if "REPORT" in line:
-            print("Ran report")
-            robots[activeRobot - 1].report()
-
-
-
-
+            activeRobot = len(robots) - 1
+        elif "MOVE" in line:
+            #print("Ran move")
+            robots[activeRobot].move()
+        elif "LEFT" in line:
+            #print("Ran left")
+            robots[activeRobot].rotate('L')
+        elif "RIGHT" in line:
+            #print("Ran right")
+            robots[activeRobot].rotate('R')
+        elif "REPORT" in line:
+            #print("Ran report")
+            robots[activeRobot].report()
+        elif "ROBOT" in line:
+            activeRobot = int(line[6])
+        else:
+            print(f"The command {line.strip()} is not valid")
 
 if __name__=="__main__":
     main()
--- Robot.py
+++ Robot.py
@@ -1,13 +1,14 @@
 import enum
 
+# The size of the table
 tableX = 5
 tableY = 5
 
 class Direction(enum.Enum):
-    North = 0
-    East = 1
-    South = 2
-    West = 3
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
 
 class Robot:
     x = None
@@ -16,38 +17,50 @@ class Robot:
 
     def place(self, x, y, dir):
         '''Place a robot on the table and return it'''
-        if 0 < x < tableX - 1: 
-            x = x
-        if 0 < y < tableY - 1: 
-            y = y
-        dir = dir
+        if 0 <= x <= tableX - 1: 
+            self.x = x
+        else:
+            print(f"Failed to place robot, {x} is off the table")
+        if 0 <= y <= tableY - 1: 
+            self.y = y
+        else:
+            print(f"Failed to place robot, {y} is off the table")
+        self.dir = Direction[dir]
     
     def rotate(self, dir):
-        '''Rotate the given robot either left or right, signified by R and L'''
+        '''Rotate the given robot either left or right, signified by R
+        and L'''
         if dir == 'R':
             newDir = Direction((self.dir.value + 1) % 4)
             self.dir = newDir
-        if dir == 'L':
+        elif dir == 'L':
             newDir = Direction((self.dir.value - 1) % 4)
             self.dir = newDir
+        else:
+            print(
+                """Failed to rotate, {dir} is not a valid direction to
+                rotate. (Please use L or R)"""
+            )
 
     def move(self):
-        '''Move the robot forward one space in the direction it is facing, making sure it doesn't fall off the table'''
-        if self.dir == Direction.North:
+        '''Move the robot forward one space in the direction it is 
+        facing, making sure it doesn't fall off the table'''
+        if self.dir == Direction.NORTH:
             if self.x != tableX - 1:
                 self.x += 1
-        if self.dir == Direction.East:
+        if self.dir == Direction.EAST:
             if self.y != tableY - 1:
                 self.y += 1
-        if self.dir == Direction.South:
+        if self.dir == Direction.SOUTH:
             if self.x != 0:
                 self.x -= 1
-        if self.dir == Direction.West:
+        if self.dir == Direction.WEST:
             if self.y != 0:
                 self.y -= 1
 
     def report(self):
-        '''Output the provided robot's number, current position and the direction it is facing'''
+        '''Output the provided robot's number, current position and the
+        direction it is facing'''
         print(f"Robot is at {self.x} {self.y}, facing {self.dir.name}")
 
             
